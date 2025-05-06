ed5eb207fa225832d099c8b00fd77f96840a6c41
Pavan Kumar BM <pavankumarbm1234@gmail.como>
2025-05-06 11:51:19 +0530

    solved

--- 01_Array/Basic_Operations/left_rotate_array_by_one_position.js
+++ 01_Array/Basic_Operations/left_rotate_array_by_one_position.js
@@ -17,3 +17,11 @@
 
 🧠 Write your implementation below this comment:
 */
+const arr = [1,2,3,4,5]
+let temp = arr[0]
+for (let i = 1; i < arr.length; i++) {
+  arr[i-1] = arr[i]
+}
+arr[arr.length-1] = temp
+console.log(arr);
+
